b4c52f66e45ad4e0123b5fd3175f804ac18a95e1
arkinem <blazejgolinski@live.com>
2020-02-16 21:25:16 +0000

    initializes express project

--- src/routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as controller from "./controller";
-
-const router = express.Router();
-
-router.get("/api/v1/airpollution", controller.getAirPollution);
-
-export default router;
