65d04418d9c6de42c30c37d4514cf3a4c9ab5994
arkinem <blazejgolinski@live.com>
2020-08-08 21:19:59 +0100

    removes unused route

--- server/routes/users.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-var router = express.Router();
-
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-	res.send("respond with a resource (+1)");
-});
-
-export default router;
