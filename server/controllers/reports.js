9490a26702783013c1fce8a7508464a848899747
arkinem <blazejgolinski@live.com>
2021-07-27 04:35:22 +0100

    Request delay fix

--- server/controllers/reports.js
+++ server/controllers/reports.js
@@ -12,15 +12,20 @@ export const generateReport = async (req, res) => {
 	const { lat, lng, postcode } = req.query;
 	let result = {};
 
+	const delayPromise = async (delay, promise) => {
+		await new Promise((resolve) => setTimeout(resolve, delay));
+		return promise;
+	};
+
 	const allOptions = {
 		airQuality: getAirQualityInformation(lat, lng),
 		crimeData: getCrimeStatistics(lat, lng),
 		councilTax: getCouncilTaxInfo(postcode),
-		demographics: getDemographicsInfo(postcode),
-		schools: getSchoolsInfo(postcode),
-		pricesPerSqf: getPricePerSqfInfo(postcode),
-		soldPricesPerSqf: getSoldPricePerSqfInfo(postcode),
-		restaurants: getRestaurantsInfo(postcode),
+		demographics: delayPromise(500, getDemographicsInfo(postcode)),
+		schools: delayPromise(1000, getSchoolsInfo(postcode)),
+		pricesPerSqf: delayPromise(1500, getPricePerSqfInfo(postcode)),
+		soldPricesPerSqf: delayPromise(2000, getSoldPricePerSqfInfo(postcode)),
+		restaurants: delayPromise(2500, getRestaurantsInfo(postcode)),
 	};
 
 	let dataToFetch = {};
